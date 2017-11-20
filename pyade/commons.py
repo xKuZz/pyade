1a421ebc009f749e6bfcdbd0d966b92ed12b35a6
xKuZz <davidcr96@gmail.com>
2017-11-20 17:51:42 +0100

    Extracting Binary Mutation to commons file Using Flake8 style in commons.py. Using docstring in commons.py

--- pyade/commons.py
+++ pyade/commons.py
@@ -1,16 +1,18 @@
 import numpy as np
+from typing import Callable, Union
 
 
-def keep_bounds(population: np.array, bounds: np.array) -> np.array:
+def keep_bounds(population: np.ndarray,
+                bounds: Union[np.ndarray, list]) -> np.ndarray:
     """
     Constrains the population to its proper limits.
     Any value outside its bounded ranged is clipped.
     :param population: Current population that may not be constrained.
-    :type population: np.array
+    :type population: np.ndarray
     :param bounds: Numpy array of tuples (min, max).
                    Each tuple represents a gen of an individual.
-    :type bounds: np.array
-    :rtype np.array
+    :type bounds: Union[np.ndarray, list]
+    :rtype np.ndarray
     :return: Population constrained within its bounds.
     """
     minimum = [bound[0] for bound in bounds]
@@ -19,18 +21,67 @@ def keep_bounds(population: np.array, bounds: np.array) -> np.array:
 
 
 def init_population(population_size: int, individual_size: int,
-                     bounds: np.array) -> np.array:
+                    bounds: Union[np.ndarray, list]) -> np.ndarray:
     """
-    Creates a random population within its constrained bounds
+    Creates a random population within its constrained bounds.
     :param population_size: Number of individuals desired in the population.
     :type population_size: int
-    :param individual_size: Number of features/gens
+    :param individual_size: Number of features/gens.
     :type individual_size: int
     :param bounds: Numpy array of tuples (min, max).
                    Each tuple represents a gen of an individual.
-    :type bounds: np.array
-    :rtype: np.array
+    :type bounds: Union[np.ndarray, list]
+    :rtype: np.ndarray
     :return: Initialized population.
     """
+    # FIXME: Adjust to real bounds
     population = np.random.randn(population_size, individual_size)
     return keep_bounds(population, bounds)
+
+
+def apply_fitness(population: np.ndarray,
+                  func: Callable[[np.ndarray], np.ndarray]) -> np.ndarray:
+    """
+    Applies the given fitness function to each individual of the population.
+    :param population: Population to apply the current fitness function.
+    :type population: np.ndarray
+    :param func: Function that is used to calculate the fitness.
+    :type func: np.ndarray
+    :rtype np.ndarray
+    :return: Numpy array of fitness for each individual.
+    """
+    return np.array([func(individual) for individual in population])
+
+
+def binary_mutation(population: np.ndarray,
+                    F: Union[int, float],
+                    bounds: Union[np.ndarray, list]) -> np.ndarray:
+    """
+    Calculate the binary mutation of the population. For each individual (n),
+    3 random parents (x,y,z) are selected. The parents are guaranteed to not
+    be in the same position than the original. New individual are created by
+    n = z + F * (x-y)
+    :param population: Population to apply the mutation
+    :type population: np.ndarray
+    :param F: Parameter of control of the mutation. Must be in [0, 2].
+    :type F: Union[int, float]
+    :param bounds: Numpy array of tuples (min, max).
+                   Each tuple represents a gen of an individual.
+    :type bounds: Union[np.ndarray, list]
+    :rtype: np.ndarray
+    :return: Mutated population
+    """
+
+    # 1. For each number, obtain 3 random integers that are not the number
+    parents = []
+    for i in range(population.shape[0]):
+        choices = np.append(np.arange(i), np.arange(i+1, population.shape[0]))
+        parents.append(np.random.choice(choices, 3, replace=False))
+
+    parents = np.matrix(parents)
+
+    # 2. Apply the formula to each set of parents
+    mutated = F * (population[parents[:, 0]] - population[parents[:, 1]])
+    mutated = population[parents[:, 2]]
+    mutated = np.reshape(mutated, population.shape)
+    return keep_bounds(mutated, bounds)
--- pyade/de.py
+++ pyade/de.py
@@ -26,44 +26,22 @@ def de(settings):
     strategy = settings.strategy
     F = settings.n_parents
     n_parents = settings.n_parents
-    max_iter = settings.max_ite
+    max_iter = settings.max_iter
     seed = settings.seed
-    def __mutate1(strategy, current_index, population, F, n_parents ):
-        # Random strategy. Pull 3 candidates out of the population different from the current one
-        if strategy == 'random':
-            candidates = np.delete(population, current_index,0)
-            candidates = candidates[np.random.randint(0,candidates.shape[0],n_parents,np.int)]
-
-        new = candidates[2] + F * (candidates[0]-candidates[1])
-        return np.array(new)
-
-    def __mutate(strategy,population, bounds, F, n_parents = 3):
-        mut = [__mutate1(strategy, individual, population, F, n_parents) for individual in range(population.shape[0])]
-        mut = np.reshape(np.array(mut),population.shape)
-        mut = pyade.commons.keep_bounds(mut,bounds)
-        return mut
-
     def __recombinate(CR, population, mutated):
         return np.array([mutated[i] if np.random.ranf() < CR else population[i] for i in range(population.shape[0])])
 
     def __selection(recombinated, r_fitness, population, fitness):
         return np.array([recombinated[i] if r_fitness[i] < fitness[i] else population[i] for i in range(population.shape[0])])
 
-    def __fitness(population):
-        return [func(individual) for individual in population]
-
     # 1. Initialization
-    population = pyade.commons.init_population(population_size, individual_size)
-
-    population = np.array(population)
-
-    # 1.2 Evaluate fitness
-    fitness = __fitness(population)
+    population = pyade.commons.init_population(population_size, individual_size, bounds)
+    fitness = pyade.commons.apply_fitness(population, func)
 
     for num_iters in range(max_iter):
-        mutated = __mutate(strategy,population,bounds,F,n_parents)
+        mutated = pyade.commons.binary_mutation(population, F, bounds)
         recombinated = __recombinate(CR, population, mutated)
-        r_fitness = [func(individual) for individual in recombinated]
+        r_fitness = pyade.commons.apply_fitness(recombinated, func)
         population = __selection(recombinated,r_fitness,population,fitness)
 
     fitness = [func(individual) for individual in population]
@@ -72,6 +50,6 @@ def de(settings):
 
 if __name__ == '__main__':
     f = lambda x : (x**2).sum()
-    settings = DE_Settings(func=f, bounds=[(-3,3),(-5,5)],population_size=50,seed = 10)
+    settings = DE_Settings(func=f, bounds=[(-3,3),(-5,5)],population_size=50, individual_size = 2,seed = 10)
     print(de(settings))
 
