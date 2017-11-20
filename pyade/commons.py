458ac00e32f9e0468c771230678be43b5a0fc917
xKuZz <davidcr96@gmail.com>
2017-11-20 15:27:43 +0100

    Extracting functions to commons file. Functions: Keep Bounds, Init Population. Using Flake8 style in commons.py. Using docstring in commons.py

---
+++ pyade/commons.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def _keep_bounds(population: np.array, bounds: np.array) -> np.array:
+    """
+    Constrains the population to its proper limits.
+    Any value outside its bounded ranged is clipped.
+    :param population: Current population that may not be constrained.
+    :type population: np.array
+    :param bounds: Numpy array of tuples (min, max).
+                   Each tuple represents a gen of an individual.
+    :type bounds: np.array
+    :rtype np.array
+    :return: Population constrained within its bounds.
+    """
+    minimum = [bound[0] for bound in bounds]
+    maximum = [bound[1] for bound in bounds]
+    return np.clip(population, minimum, maximum)
+
+
+def _init_population(population_size: int, individual_size: int,
+                     bounds: np.array) -> np.array:
+    """
+    Creates a random population within its constrained bounds
+    :param population_size: Number of individuals desired in the population.
+    :type population_size: int
+    :param individual_size: Number of features/gens
+    :type individual_size: int
+    :param bounds: Numpy array of tuples (min, max).
+                   Each tuple represents a gen of an individual.
+    :type bounds: np.array
+    :rtype: np.array
+    :return: Initialized population.
+    """
+    population = np.random.randn(population_size, individual_size)
+    return _keep_bounds(population, bounds)
--- pyade/de.py
+++ pyade/de.py
@@ -1,37 +1,36 @@
 import numpy as np
-import collections
+import pyade.commons
 
 class DE_Settings:
-    def __init__(self, func, bounds, population_size, GR = 0.3, strategy = 'random', F = None, n_parents = 3,
+    def __init__(self, func, bounds, population_size, CR = 0.3, strategy ='random', F = None, n_parents = 3,
                  max_iter = 10000, seed = None):
         self.func = func # Function for fitness
         self.bounds = bounds # Bounds for each feature of the poblation
         self.population_size = population_size # Size of the population
-        self.GR = GR # Recombination chance in [0.0,1.0]
+        self.CR = CR # Recombination chance in [0.0,1.0]
         self.strategy = strategy # Strategy selected for mutation
         self.F = F # Mutation parameter in [0,2]
         self.n_parents = n_parents # Number of parents for mutation
         self.max_iter = max_iter # Termination criteria: Maximum number of genereations
         self.seed = seed # Seed for random generation
 
-def de(settings):
+def de(**settings):
     # Extract from namedtuple
     func = settings.func
     bounds = settings.bounds
     population_size = settings.population_size
-    GR = settings.GR
+    individual_size = settings.individual_size
+    CR = settings.CR
     strategy = settings.strategy
     F = settings.n_parents
     n_parents = settings.n_parents
-    max_iter = settings.max_iter
+    max_iter = settings.max_ite
     seed = settings.seed
     def __mutate1(strategy, current_index, population, F, n_parents ):
         # Random strategy. Pull 3 candidates out of the population different from the current one
         if strategy == 'random':
             candidates = np.delete(population, current_index,0)
             candidates = candidates[np.random.randint(0,candidates.shape[0],n_parents,np.int)]
-        if F == None:
-            F = 2 * np.random.random_sample()
 
         new = candidates[2] + F * (candidates[0]-candidates[1])
         return np.array(new)
@@ -39,37 +38,20 @@ def de(settings):
     def __mutate(strategy,population, bounds, F, n_parents = 3):
         mut = [__mutate1(strategy, individual, population, F, n_parents) for individual in range(population.shape[0])]
         mut = np.reshape(np.array(mut),population.shape)
-        mut = __keep_bounds(mut,bounds)
+        mut = pyade.commons._keep_bounds(mut,bounds)
         return mut
 
-    def __keep_bounds(population, bounds):
-        for i in range(len(bounds)):
-            population[:,i] = np.clip(population[:,i],bounds[i][0], bounds[i][1])
-        return population
-
-    def __recombinate(GR, population, mutated):
-        return np.array([mutated[i] if np.random.ranf() < GR else population[i] for i in range(population.shape[0])])
+    def __recombinate(CR, population, mutated):
+        return np.array([mutated[i] if np.random.ranf() < CR else population[i] for i in range(population.shape[0])])
 
     def __selection(recombinated, r_fitness, population, fitness):
         return np.array([recombinated[i] if r_fitness[i] < fitness[i] else population[i] for i in range(population.shape[0])])
 
     def __fitness(population):
         return [func(individual) for individual in population]
-    # 1. Initialization: Start a random population
-    # 1.1 Create an array: each column represents an attribute
-    #     First row is the minimum and second row is the maximum.
-    limits = np.array(bounds, dtype='float').T
-    scale = np.subtract(limits[1], limits[0])
 
-    # 1.2 Set the seed for the algorithm
-    np.random.seed(seed)
-
-    # 1.3 Choose a random value for each attribute within its bounds
-    population = []
-    for i in range(population_size):
-        factor = np.random.random_sample(len(bounds))
-        individual = np.multiply(scale, factor)
-        population.append(np.add(individual, limits[0]))
+    # 1. Initialization
+    population = pyade.commons._init_population(population_size, individual_size)
 
     population = np.array(population)
     # 1.4 Evaluate fitness
@@ -77,7 +59,7 @@ def de(settings):
 
     for num_iters in range(max_iter):
         mutated = __mutate(strategy,population,bounds,F,n_parents)
-        recombinated = __recombinate(GR, population, mutated)
+        recombinated = __recombinate(CR, population, mutated)
         r_fitness = [func(individual) for individual in recombinated]
         population = __selection(recombinated,r_fitness,population,fitness)
 
@@ -86,7 +68,7 @@ def de(settings):
     return population[best]
 
 if __name__ == '__main__':
-    f = lambda x : x[0]**2+x[1]**2
+    f = lambda x : (x**2).sum()
     settings = DE_Settings(func=f, bounds=[(-3,3),(-5,5)],population_size=50,seed = 10)
     print(de(settings))
 
