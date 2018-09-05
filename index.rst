f5efe7172df19ea6a107094f4a2be51c54cba452
xKuZz <davidcr96@gmail.com>
2018-09-05 18:39:21 +0200

    Last changes and added documentation files

---
+++ index.rst
@@ -0,0 +1,20 @@
+.. PyaDE documentation master file, created by
+   sphinx-quickstart on Sun Sep  2 23:38:53 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PyaDE's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
+import setuptools
 
 setup(
-    name='PyADE',
-    version='1.0dev',
+    name='pyade-python',
+    version='1.0',
     packages=['pyade'],
-    url='http://',
+    url='https://github.com/xKuZz/pyade',
     license='MIT',
     author='xKuZz',
     author_email='davidcr96@gmail.com',
     description='A Python Advanced Differential Evolution algorithms library',
-    install_requires=['numpy', 'pytest', 'scipy']
 )
