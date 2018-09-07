edc2154d596cbd7508177883d5e24d33e178be19
David Criado Ramón <xKuZz@users.noreply.github.com>
2018-09-07 18:50:43 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -195,3 +195,27 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+def run_apidoc(_):
+    modules = ['pyade/',
+              'pyade/de.py']
+    
+    for module in modules:
+        cur_dir = os.path.abspath(os.path.dirname(__file__))
+        output_path = os.path.join(cur_dir, module, 'doc')
+        cmd_path = 'sphinx-apidoc'
+        if hasattr(sys, 'real_prefix'):  # Check to see if we are in a virtualenv
+            # If we are, assemble the path manually
+            cmd_path = os.path.abspath(os.path.join(sys.prefix, 'bin', 'sphinx-apidoc'))
+        main([cmd_path, '-e', '-o', output_path, module, '--force'])
+    from sphinx.apidoc import main
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    module = '.'
+    output_path = os.path.join(cur_dir, 'source')
+    main(['-e', '-o', output_path, module, '--force'])
+    
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
