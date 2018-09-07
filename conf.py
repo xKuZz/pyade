ca0e25eb087fec4147e289021131dffcfd33ee9d
David Criado Ramón <xKuZz@users.noreply.github.com>
2018-09-07 18:54:51 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -201,7 +201,7 @@ def run_apidoc(_):
     
     for module in modules:
         cur_dir = os.path.abspath(os.path.dirname(__file__))
-        output_path = os.path.join(cur_dir, module, 'doc')
+        output_path = os.path.join(cur_dir, module, '.')
         cmd_path = 'sphinx-apidoc'
         if hasattr(sys, 'real_prefix'):  # Check to see if we are in a virtualenv
             # If we are, assemble the path manually
